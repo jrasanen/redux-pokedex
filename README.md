4e96a06a3e35004fadad4c21d60251a5dc742916
Jussi Räsänen <jussir@gmail.com>
2016-09-18 19:33:18 +0300

    Changed readme

--- README.md
+++ README.md
@@ -1,5 +1,4 @@
-[![js-standard-style](https://img.shields.io/badge/code%20style-standard-brightgreen.svg?style=flat)](http://standardjs.com/)
 
 1. `npm install`
-2. `npm start`
-3. open http://localhost:8000/ in the browser
+2. `npm build`
+3. `npm start`
--- server.js
+++ server.js
@@ -32,7 +32,8 @@ function handleRender(req, res) {
 function renderFullPage(html, preloadedState) {
   return `<html>
   	<head>
-  		<title>redux</title>
+      <meta charset="UTF-8">
+  		<title>pokedex</title>
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
       <style>
         body {
